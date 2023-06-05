9e623deee58c80607527978b2e2c31ae4a56ba44
Vladix974 <lutsenkovladislav974@gmail.com>
2023-06-05 20:12:19 +0300

    add friendsScreenFragment

---
+++ app/src/main/java/com/example/cnc/presentation/friendsScreen/friendsScreenFragment.kt
@@ -0,0 +1,7 @@
+package com.example.cnc.presentation.friendsScreen
+
+import androidx.fragment.app.Fragment
+import com.example.cnc.R
+
+class friendsScreenFragment :Fragment(R.layout.fragment_friends_screen) {
+}
