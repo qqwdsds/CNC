1d8777983871a3bb29c53293845244c41724815f
qqweq <maximligomina007@gmail.com>
2023-06-05 16:38:31 +0300

    Design
    Added navigation and created HomeScreenFragment. Added a new font style
    for NoMesages message in HomeScreenFragment. Added simple layout for rv
    item.

--- app/src/main/java/com/example/cnc/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cnc/presentation/MainActivity.kt
@@ -1,7 +1,7 @@
-package com.example.cnc
+package com.example.cnc.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.cnc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
---
+++ app/src/main/java/com/example/cnc/presentation/homeScreenFragment/HomeScreenFragment.kt
@@ -0,0 +1,8 @@
+package com.example.cnc.presentation.homeScreenFragment
+
+import androidx.fragment.app.Fragment
+import com.example.cnc.R
+
+class HomeScreenFragment: Fragment(R.layout.fragment_home_screen) {
+
+}
