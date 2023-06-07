bee5d14e641675d06ec6e0aa54cca1b15d6967b1
Vladix974 <lutsenkovladislav974@gmail.com>
2023-06-07 16:06:01 +0300

    add menu registration & sign

---
+++ app/src/main/java/com/example/cnc/presentation/registrationScreen/RegistrationScreenFragment.kt
@@ -0,0 +1,8 @@
+package com.example.cnc.presentation.registrationScreen
+
+import androidx.fragment.app.Fragment
+import com.example.cnc.R
+
+class RegistrationScreenFragment :Fragment(R.layout.fragment_registration_screen){
+
+}
---
+++ app/src/main/java/com/example/cnc/presentation/signScreen/SignScreenFragment.kt
@@ -0,0 +1,8 @@
+package com.example.cnc.presentation.signScreen
+
+import androidx.fragment.app.Fragment
+import com.example.cnc.R
+
+class SignScreenFragment : Fragment(R.layout.fragment_sign_screen) {
+
+}
