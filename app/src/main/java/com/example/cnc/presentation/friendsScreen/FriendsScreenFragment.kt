2531130058c5a6b2a123032c691f2c90046d48e5
qqwdsds <98281464+qqwdsds@users.noreply.github.com>
2023-06-07 14:55:41 +0300

    Transition between screens
    Added transition between screens, close button functionality

--- app/src/main/java/com/example/cnc/presentation/friendsScreen/FriendsScreenFragment.kt
+++ app/src/main/java/com/example/cnc/presentation/friendsScreen/FriendsScreenFragment.kt
@@ -1,7 +1,28 @@
 package com.example.cnc.presentation.friendsScreen
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.cnc.R
+import com.example.cnc.databinding.FragmentFriendsScreenBinding
 
 class FriendsScreenFragment :Fragment(R.layout.fragment_friends_screen) {
+    private lateinit var binding: FragmentFriendsScreenBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentFriendsScreenBinding.inflate(inflater)
+
+        return binding.root
+    }// end onCreateView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.closeWindowButton.setOnClickListener{
+            findNavController().popBackStack()
+        }
+    }// end onViewCreated
 }
