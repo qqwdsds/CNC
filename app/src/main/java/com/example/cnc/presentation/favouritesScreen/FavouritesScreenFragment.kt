e90a195a9f5db1e4b8f999b5ddd24f11bd66f811
Vladix974 <lutsenkovladislav974@gmail.com>
2023-06-07 12:06:10 +0300

    add FavouritesScreenFragment

---
+++ app/src/main/java/com/example/cnc/presentation/favouritesScreen/FavouritesScreenFragment.kt
@@ -0,0 +1,7 @@
+package com.example.cnc.presentation.favouritesScreen
+
+import androidx.fragment.app.Fragment
+import com.example.cnc.R
+
+class FavouritesScreenFragment : Fragment(R.layout.fragment_favourite_sceen) {
+}
