a1f0b5de1f304204b3afa15f931f12572e546c09
Vladix974 <lutsenkovladislav974@gmail.com>
2023-06-07 12:24:49 +0300

    change friendsScreenFragment

--- app/src/main/java/com/example/cnc/presentation/friendsScreen/FriendsScreenFragment.kt
+++ app/src/main/java/com/example/cnc/presentation/friendsScreen/FriendsScreenFragment.kt
@@ -3,5 +3,5 @@ package com.example.cnc.presentation.friendsScreen
 import androidx.fragment.app.Fragment
 import com.example.cnc.R
 
-class friendsScreenFragment :Fragment(R.layout.fragment_friends_screen) {
+class FriendsScreenFragment :Fragment(R.layout.fragment_friends_screen) {
 }
